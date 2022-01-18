8f5a5f86f3f12ea7aefdb4d5b5ed2521f9772f9a
D0mmik <dominik.strnadel@gmail.com>
2022-01-18 21:16:20 +0100

    Shooting

---
+++ SpaceShooterDommik/Assets/Scripts/shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{   public GameObject bullet;
+    private GameObject newbullet;
+    public float bulletspeed = 1500f;
+    public Transform shootingPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            newbullet = Instantiate(bullet,shootingPoint.position,Quaternion.identity);
+            newbullet.AddComponent<Rigidbody>();
+            newbullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletspeed);
+            Destroy (newbullet, 2);
+            
+        }
+    }
+}
